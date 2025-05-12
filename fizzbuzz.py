2ce66594f99c1b8587ae4d9b6889e354f367da80
SATO Daisuke <densuke@fuga.jp>
2025-05-12 08:25:54 +0000

    初期コミット

---
+++ fizzbuzz.py
@@ -0,0 +1,34 @@
+
+def fizzbuzz(n: int) -> str:
+    """いわゆるFizzBuzz問題を解きます
+
+    引数で渡された値に対し、以下のような文字列を返します。
+
+    - nが3の倍数のときは"Fizz"
+    - nが5の倍数のときは"Buzz"
+    - nが3の倍数かつ5の倍数のときは"FizzBuzz"
+    - それ以外のときはnをそのまま返します。
+
+    >>> fizzbuzz(1)
+    '1'
+
+    >>> fizzbuzz(3)
+    'Fizz'
+
+    >>> fizzbuzz(5)
+    'Buzz'
+
+    >>> fizzbuzz(15)
+    'FizzBuzz'
+    """
+    pass
+
+
+def runtest():
+    print("テストを実施します")
+    import doctest
+    doctest.testmod()
+
+
+if __name__ == "__main__":
+    runtest()
---
+++ main.py
@@ -0,0 +1,9 @@
+from fizzbuzz import fizzbuzz
+
+def main():
+    print("FizzBuzzを実行します")
+    for i in range(1, 16):
+        print(fizzbuzz(i))
+
+if __name__ == "__main__":
+    main()
